2cc7a3b7e0994f4d6c26e4ada1c2d35ccf8df2b5
Tri Luong <lxt0094@gmail.com>
2019-01-13 15:56:08 +0700

    Add components to reactjs
    Note: use react 15.4 or later.
    
    Signed-off-by: Tri Luong <lxt0094@gmail.com>

--- ex01/public/process.js
+++ ex01/public/process.js
@@ -1,7 +1,16 @@
+class HelloWorld extends React.Component{
+    render(){
+        return(
+            <h1 className="yellowColor">Hello World!!!</h1>
+        );
+    }
+}
+
 ReactDOM.render(
     <div>
         <h1>Hello World!!!</h1>
-        <h1 className="yellowColor">Hello World!!!</h1>
+        <HelloWorld></HelloWorld>
+        <HelloWorld />
     </div>
     , document.getElementById("root")
 );
